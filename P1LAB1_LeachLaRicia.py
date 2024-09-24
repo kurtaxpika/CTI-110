d07501d78a4899b79d1051173546e08f93a3a666
kurtaxpika <laricialeach@gmail.com>
2024-09-24 14:24:16 -0400

    First upload!

---
+++ P1LAB1_LeachLaRicia.py
@@ -0,0 +1,13 @@
+# LaRicia Leach
+# 9/24/2024
+# P1LAB1
+# get input from the user and display output.
+# Get first name from user
+
+first_name =input("Enter your first name: ")
+
+#Get the last name from user
+last_name =input ("Enter your last name: ")
+
+# Display info to user
+print("Hello,", first_name, last_name + "!Welcome to CTI-110", )
