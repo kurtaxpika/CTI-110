d2844b0a459b1e197f2706a9b134e3bd2cc3f0a5
kurtaxpika <laricialeach@gmail.com>
2024-09-26 13:50:12 -0400

    Add files via upload
    P1HW1 !!

---
+++ P1HW1_LeachLaRicia.py
@@ -0,0 +1,9 @@
+# LaRicia Leach
+# 9/24/2024
+# P1HW1
+# Get integer input from user and perform math calculations
+
+# Display output to uder
+print("---------Calculating Exponents---------")
+print()
+print()
