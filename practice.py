50f7a3663a09965049209318dc62d4f423e658e0
kurtaxpika <laricialeach@gmail.com>
2024-10-03 13:37:14 -0400

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,11 @@
+# Laricia Leach
+
+# Get value of the sides from the user
+side1 = float(input("Enter value for side 1:"))
+side2= float(input("Enter value for side 2:"))
+
+# Calculate the hyoptenuse
+hypotenuse= (side1 ** 2) + (side2 ** 2)
+
+# Display to the user
+print(f"The hypotenuse of a right triangle with the sides {side1} and {side2} is {hyoptenuse}")
